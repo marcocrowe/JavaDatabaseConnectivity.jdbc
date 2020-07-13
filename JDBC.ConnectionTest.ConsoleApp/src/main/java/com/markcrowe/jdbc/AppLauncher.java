394068056f9b55ec4b75a83b02b2be9e6c784dd7
Mark Crowe <markcrowe-com@users.noreply.github.com>
2020-07-13 04:54:33 +0100

    App Launcher

---
+++ JDBC.ConnectionTest.ConsoleApp/src/main/java/com/markcrowe/jdbc/AppLauncher.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020 Mark Crowe <https://github.com/markcrowe-com>. All rights reserved.
+ */
+package com.markcrowe.jdbc;
+
+/**
+ * Class with main method to launch the application
+ */
+public class AppLauncher
+{
+	public static void main(String[] args)
+	{
+		ConsoleApp consoleApp = new ConsoleApp();
+		consoleApp.run();
+	}
+}
