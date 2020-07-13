6413c7145d182a31e5c3e99617bf83729a72751c
Mark Crowe <markcrowe-com@users.noreply.github.com>
2020-07-13 04:53:14 +0100

    SQL Exception Error Codes

---
+++ JDBC/src/main/java/com/markcrowe/jdbc/SQLExceptionErrorCodes.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 Mark Crowe <https://github.com/markcrowe-com>. All rights reserved.
+ */
+package com.markcrowe.jdbc;
+
+/**
+ * SQLException Error Code definitions
+ */
+public class SQLExceptionErrorCodes
+{
+	public static final int AccessDenied = 1044;
+	public static final int AuthenticationError = 1045;
+	public static final int MissingDriver = 0;
+	public static final int UnknownDatabase = 1049;
+}
---
+++ JDBC/src/main/java/com/markcrowe/jdbc/SQLExceptionSuggestedFixes.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020 Mark Crowe <https://github.com/markcrowe-com>. All rights reserved.
+ */
+package com.markcrowe.jdbc;
+
+/**
+ * SQLException Suggested Fixes
+ */
+public class SQLExceptionSuggestedFixes
+{
+	public static final String AccessDenied = "Check user permissions for '%s' on database.";
+	public static final String AuthenticationError = "Check username and password.";
+	public static final String MissingDriver = "Check JDBC application and then check project dependancies. eg. mysql and mysql-connector-java.";
+	public static final String UndefinedErrorCode = "Undefined error code. You are on your own.";
+	public static final String UnknownDatabase = "Check database name.";
+	//
+	//	Public Static Methods
+	//
+	/**
+	 * Gets a message that suggests how to fix for the error.
+	 *
+	 * @param sqlErrorCode
+	 * @param username
+	 * @return
+	 */
+	public static String getSuggestedFixMessage(int sqlErrorCode, String username)
+	{
+		switch(sqlErrorCode)
+		{
+			case SQLExceptionErrorCodes.AccessDenied:
+				return String.format(AccessDenied, username);
+			case SQLExceptionErrorCodes.AuthenticationError:
+				return AuthenticationError;
+			case SQLExceptionErrorCodes.MissingDriver:
+				return MissingDriver;
+			case SQLExceptionErrorCodes.UnknownDatabase:
+				return UnknownDatabase;
+			default:
+				return UndefinedErrorCode;
+		}
+	}
+}
