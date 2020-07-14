365cffb4f2a87ea158489384223a7db5b1afd8e9
Mark Crowe <markcrowe-com@users.noreply.github.com>
2020-07-14 03:10:49 +0100

    New Project for Windows App

---
+++ JDBC.ConnectionTest.WindowsApp/src/main/java/com/markcrowe/jdbc/AppLauncher.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 Mark Crowe <https://github.com/markcrowe-com>. All rights reserved.
+ */
+package com.markcrowe.jdbc;
+
+/**
+ * Class with main method to launch the application
+ */
+class AppLauncher
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Building Windows App");
+	}
+}
